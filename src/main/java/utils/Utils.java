156e8400e2cdb848a065cda4c7d64223fc80b107
rotem <rotem8zaif@gmail.com>
2021-09-24 09:06:25 +0300

    added readProperty() method for getting properties values

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,6 +1,34 @@
 package utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
- *
+ * Utils class as an assistance class which includes methods to read/write data from files
  * */
 public class Utils {
+
+    private static FileInputStream fis;
+
+    /**
+     * this method reads a property from a config.properties file and return its value
+     * @param - a String key property name
+     * @return - property value
+     * */
+    public static String readProperty(String key){
+        String value = "";
+        try {
+            fis = new FileInputStream("./src/com/stevensmith/data/configuration.properties");
+            Properties prop = new Properties();
+            prop.load(fis); // load a properties file
+            value = prop.getProperty(key); // get the property value
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
 }

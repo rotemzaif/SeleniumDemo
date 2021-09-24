25658d731616dd7488154e3ccac820b4a86d8198
rotem <rotem8zaif@gmail.com>
2021-09-24 10:22:09 +0300

    added class documentation

--- src/main/java/pageObjects/WelcomePage.java
+++ src/main/java/pageObjects/WelcomePage.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
-
+/**
+ * WelcomePage class is the home page which consists of links to other pages.
+ * It consists of a list of all links and a method which clicks on a given link
+ * */
 public class WelcomePage extends BasePage {
     // page elements
     @FindBy(css = "ul > li > a")
@@ -19,6 +22,10 @@ public class WelcomePage extends BasePage {
         return linksList;
     }
 
+    /**
+     * A method which clicks on a link
+     * @Param - A string link name
+     * */
     public void goToPage(String name){
         for (WebElement link : linksList) {
             if(getText(link).equals(name)){

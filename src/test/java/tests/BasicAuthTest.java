b61ea21b839b1a30c9b79e2a6b4509435f93a2ae
rotem <rotem8zaif@gmail.com>
2021-09-28 20:51:41 +0300

    fixed existing login success tests and added more tests to cover login with invalid credentials and login cancel

--- src/test/java/tests/BasicAuthTest.java
+++ src/test/java/tests/BasicAuthTest.java
@@ -1,38 +1,80 @@
 package tests;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.BasicAuthPage;
 import pageObjects.WelcomePage;
 import utils.Utils;
+import java.io.IOException;
 
 public class BasicAuthTest extends BaseTest {
     // test objects
+    WelcomePage wp;
     BasicAuthPage bap ;
     @Test
     public void tc01_check_home_page_display(){
-        WelcomePage wp = new WelcomePage(driver);
-        heading = Utils.readProperty("wpTitle");
+        wp = new WelcomePage(driver);
+        expectedHeading = Utils.readProperty("wpTitle");
         pageName = "Welcome";
-        Assert.assertTrue(wp.isPageDisplayed(wp.getPageHeading(),heading),pageName + " page is not loaded");
+        Assert.assertTrue(wp.isPageDisplayed(wp.getPageHeading(),expectedHeading),pageName + " page is not loaded");
     }
 
-    @Test(dependsOnMethods = "tc01_check_home_page_display")
-    public void tc02_go_to_Basic_Auth_page_with_valid_credentials() throws InterruptedException {
-        WelcomePage wp = new WelcomePage(driver);
+    @Test(description = "moving to Basic Auth page, entering valid credentials and verifying that page is displayed", dependsOnMethods = "tc01_check_home_page_display")
+    public void tc02_go_to_Basic_Auth_page_with_valid_credentials() throws InterruptedException, IOException {
+        wp = new WelcomePage(driver);
         pageName = Utils.readProperty("bapName");
         wp.goToPage(pageName);
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-//
-////        bap = new BasicAuthPage(driver);
-//        for (String str : driver.getWindowHandles()) {
-//            System.out.println(str);
-//            driver.switchTo().window(str);
-//        }
-//        Alert alert = driver.switchTo().alert();
+        // enter valid credentials (admin/admin)
+        wp.login("success");
+        Thread.sleep(2000);
+        bap = new BasicAuthPage(driver, "success");
+        expectedHeading = Utils.readProperty("bapHeading");
+        Assert.assertTrue(bap.isPageDisplayed(bap.getPageHeading(),expectedHeading), pageName + " page is not loaded");
+    }
+
+    @Test(description = "verifying that Congratulations statement is displayed", dependsOnMethods = "tc02_go_to_Basic_Auth_page_with_valid_credentials")
+    public void tc03_Basic_Auth_Page_check_success_statement(){
+        bap = new BasicAuthPage(driver, "success");
+        String expectedLabel = Utils.readProperty("successStatement");
+        Assert.assertEquals(bap.getconfirmationLabelText(), expectedLabel, "Expected 'Congratulations ....' label is not displayed");
+    }
+
+    @Test(description = "opening chrome settings and clearing its password cache in the last hour", dependsOnMethods = "tc03_Basic_Auth_Page_check_success_statement")
+    public void tc04_clear_broswer_cache_after_successful_login() throws InterruptedException {
+        bap = new BasicAuthPage(driver, "success");
+        bap.goToWelcomePage();
+        Thread.sleep(1000);
+        wp = new WelcomePage(driver);
+        wp.clearCash(driver,Utils.readProperty("homePageURL"));
+        wp = new WelcomePage(driver);
+        Thread.sleep(2000);
+        expectedHeading = Utils.readProperty("wpTitle");
+        pageName = "Welcome";
+        Assert.assertTrue(wp.isPageDisplayed(wp.getPageHeading(),expectedHeading),pageName + " page is not loaded");
+    }
+
+    @Test(description = "logining with invalid credentials and verifying that confirmation not authorization labels are displayed", dependsOnMethods = "tc04_clear_broswer_cache_after_successful_login")
+    public void tc05_Basic_Auth_Page_login_with_invalid_credentials() throws InterruptedException, IOException{
+        wp = new WelcomePage(driver);
+        pageName = Utils.readProperty("bapName");
+        wp.goToPage(pageName);
+        wp.login("invalidCredentials");
+        Thread.sleep(2000);
+        bap = new BasicAuthPage(driver,"");
+        Assert.assertFalse(bap.isConfrimationLabelExist() && bap.isAuthorizationLabelDisplayed(),
+                "confirmation or authorization statements are displayed when loging with invalid credentials");
+    }
+
+    @Test(description = "canceling loging attempt and verifying that authorization statements is displayed", dependsOnMethods = "tc05_Basic_Auth_Page_login_with_invalid_credentials")
+    public void tc06_Basic_Auth_Page_login_cancel()throws InterruptedException, IOException{
+        driver.get(Utils.readProperty("homePageURL"));
+        wp = new WelcomePage(driver);
+        pageName = Utils.readProperty("bapName");
+        wp.goToPage(pageName);
+        wp.login("cancel");
+        Thread.sleep(2000);
+        bap = new BasicAuthPage(driver,"cancel");
+        String expectedStatement = Utils.readProperty("cancelStatement");
+        Assert.assertEquals(bap.getAuthorizationText(), expectedStatement);
     }
 }

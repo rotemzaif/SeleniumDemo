cecf0b5d15a9a0a774b3f503eef166f42a7537eb
rotem <rotem8zaif@gmail.com>
2021-09-28 20:48:33 +0300

    created and added basic auth page class with all its elements objects and methods

--- src/main/java/pageObjects/BasicAuthPage.java
+++ src/main/java/pageObjects/BasicAuthPage.java
@@ -1,19 +1,57 @@
 package pageObjects;
 
-import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.Utils;
 
+/**
+ * BasicAuthPage class derives from BasicPage class with all its variables, objects and methods.
+ * The class includes and 2 label elements and methods that support getting text and validating elements existance
+ * and display
+ */
 public class BasicAuthPage extends BasePage {
-    public BasicAuthPage(WebDriver driver) {
+    // page elements
+    WebElement confirmationLabel;
+    WebElement authorizationLabel;
+
+    public BasicAuthPage(WebDriver driver, String loginType) {
         super(driver);
+        switch (loginType){
+            case "success":
+                pageHeading = driver.findElement(By.cssSelector(".example > h3"));
+                confirmationLabel = driver.findElement(By.cssSelector(".example > p"));
+                break;
+            case "cancel":
+                authorizationLabel = driver.findElement(By.cssSelector("body"));
+                break;
+        }
     }
 
-    public void enterCredentials(String user, String pswd){
-//        allertSendText(user);
-//        sleep(500);
-//        allertSendText(pswd);
-        js.executeScript("window.confirm");
-        Alert confirmation = driver.switchTo().alert();
-        System.out.println(confirmation.getText());
+    // page getters
+    public String getconfirmationLabelText(){
+        return getText(confirmationLabel);
     }
+
+    public String getAuthorizationText(){
+        return getText(authorizationLabel);
+    }
+
+    // page actions
+    public void goToWelcomePage(){
+        driver.get(Utils.readProperty("homePageURL"));
+    }
+
+    // validation methods
+    public boolean isConfrimationLabelExist(){
+        if(driver.findElements(By.cssSelector(".example > p")).isEmpty())
+            return false;
+        else return true;
+    }
+    public boolean isAuthorizationLabelDisplayed(){
+        if(driver.findElement(By.cssSelector("body")).getText().isEmpty())
+            return false;
+        else return true;
+    }
+
 }

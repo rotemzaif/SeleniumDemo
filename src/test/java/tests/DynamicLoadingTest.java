4af5246fdb55ab193ff8c94542acea5f74dd726b
rotem <rotem8zaif@gmail.com>
2021-09-28 15:13:38 +0300

    modified variable name

--- src/test/java/tests/DynamicLoadingTest.java
+++ src/test/java/tests/DynamicLoadingTest.java
@@ -15,28 +15,28 @@ public class DynamicLoadingTest extends BaseTest {
     @Test
     public void tc01_check_home_page_display(){
         WelcomePage wp = new WelcomePage(driver);
-        heading = Utils.readProperty("wpTitle");
+        expectedHeading = Utils.readProperty("wpTitle");
         pageName = "Welcome";
-        Assert.assertTrue(wp.isPageDisplayed(wp.getPageHeading(),heading),pageName + " page is not loaded");
+        Assert.assertTrue(wp.isPageDisplayed(wp.getPageHeading(),expectedHeading),pageName + " page is not loaded");
     }
 
     @Test(dependsOnMethods = "tc01_check_home_page_display")
     public void tc02_go_to_DynamicLoading_Main_page(){
         WelcomePage wp = new WelcomePage(driver);
         pageName = Utils.readProperty("dlmpName");
-        heading = Utils.readProperty("dlmpHeading");
+        expectedHeading = Utils.readProperty("dlmpHeading");
         wp.goToPage(pageName);
         dlmp = new DynamicLoadingMainPage(driver);
-        Assert.assertTrue(dlmp.isPageDisplayed(dlmp.getPageHeading(),heading),pageName + " page is not loaded");
+        Assert.assertTrue(dlmp.isPageDisplayed(dlmp.getPageHeading(),expectedHeading),pageName + " page is not loaded");
     }
 
     @Test(dependsOnMethods = "tc02_go_to_DynamicLoading_Main_page")
     public void tc03_go_to_DynamicLoading_Ex2_page(){
         dlmp = new DynamicLoadingMainPage(driver);
-        heading = Utils.readProperty("dl2pHeading");
-        dlmp.goToPage(heading);
+        expectedHeading = Utils.readProperty("dl2pHeading");
+        dlmp.goToPage(expectedHeading);
         dl2p = new DynamicLoadingEx2Page(driver);
-        Assert.assertTrue(dl2p.isPageDisplayed(dl2p.getPageHeading(),heading),pageName + " page is not loaded");
+        Assert.assertTrue(dl2p.isPageDisplayed(dl2p.getPageHeading(),expectedHeading),pageName + " page is not loaded");
     }
 
     @Test(description = "clicking on 'start' button and verifying that 'Hello World! is displayed", dependsOnMethods = "tc03_go_to_DynamicLoading_Ex2_page")
